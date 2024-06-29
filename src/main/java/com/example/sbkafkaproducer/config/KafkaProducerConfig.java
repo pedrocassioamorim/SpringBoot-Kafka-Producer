0c15e914fc068728c9bb00faa4dc1152dc2f3b92
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-29 11:48:55 -0300

    Kafka Topic Builder, simple instantiating

--- src/main/java/com/example/sbkafkaproducer/config/KafkaProducerConfig.java
+++ src/main/java/com/example/sbkafkaproducer/config/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.example.sbkafkaproducer.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -39,6 +41,15 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public NewTopic pagamentoRequestTopicBuilder(){
+        return TopicBuilder
+                .name(pagamentoRequestTopic)
+                .partitions(1)
+                .replicas(1)
+                .build();
     }
 
 
