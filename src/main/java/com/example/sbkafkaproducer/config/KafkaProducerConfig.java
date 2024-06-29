d3b9d1411da13290a017f42c0ba1fce74f4abf94
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-29 11:45:41 -0300

    Kafka Producer Factory Bean

--- src/main/java/com/example/sbkafkaproducer/config/KafkaProducerConfig.java
+++ src/main/java/com/example/sbkafkaproducer/config/KafkaProducerConfig.java
@@ -1,9 +1,19 @@
 package com.example.sbkafkaproducer.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -13,5 +23,18 @@ public class KafkaProducerConfig {
 
     @Value("${topicos.pagamento.request.topic.v1}")
     private String pagamentoRequestTopic;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:8080,broker2:9090");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Map<String, Object> props = properties.stringPropertyNames().stream()
+                .collect(Collectors.toMap(key -> key, properties::get));
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+
 
 }
