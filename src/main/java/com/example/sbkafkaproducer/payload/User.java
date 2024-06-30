4d18cb6ce03476d49dfbd0f82e252f8b8332f77b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-30 19:51:23 -0300

    Creating a class to be send as message

---
+++ src/main/java/com/example/sbkafkaproducer/payload/User.java
@@ -0,0 +1,16 @@
+package com.example.sbkafkaproducer.payload;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+
+
+}
